e9e9b5bc8781b32ee6fd6d6c153a102380fc5e83
rabia <rabiakorkmaz079@gmail.com>
2021-11-21 14:59:11 +0300

    Add project files.

---
+++ FirstMvcProject/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using FirstMvcProject.Entity;
+using FirstMvcProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvcProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            var catgoryList = _db.Category;
+
+            return View(catgoryList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category model)
+        {
+            _db.Category.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstMvcProject/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvcProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Name",TypeName ="varchar")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
